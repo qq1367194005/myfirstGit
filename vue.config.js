0e5d055d8af8d451f710838837a4c524b0c77dcb
lhy <1367194005@qq.com>
2019-07-11 13:53:00 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    //...
-    publicPath: './',
-    //...
-    devServer: {
-        // 设置代理
-        proxy: {
-          "/api": {
-            target: "http://shop.sirentoy.com", // 访问数据的计算机域名
-            changOrigin: true //开启代理
-          }
-        }
+  //...
+  publicPath: './',
+  //...
+  devServer: {
+    // 设置代理
+    proxy: {
+      "/api": {
+        target: "http://shop.sirentoy.com", // 访问数据的计算机域名
+        changOrigin: true //开启代理
       }
+    }
+  },
+  devServer: {
+    port: 3000, // 端口
+  },
 }
